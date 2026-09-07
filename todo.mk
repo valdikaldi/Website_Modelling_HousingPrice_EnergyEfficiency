ec9b401ad3b1bebf29a37012e3ad07f73c60e420
Valdimar <56283052+valdikaldi@users.noreply.github.com>
2026-09-07 21:45:24 +0200

    update readme file and add todo list

---
+++ todo.mk
@@ -0,0 +1,26 @@
+
+# To do list 
+
+- [ ] Finish setting up chapters 
+    - [x] 6.1 Main results 
+        - [x] Add the text from paper 
+        - [x] Add regression results table or just image  
+    - [x] 6.2 Market segment 
+        - [x] Add the text from paper 
+        - [x] Add regression results table or just image  
+    - [x] 6.3 Policy effect  
+        - [x] Add the text from paper 
+        - [x] Add regression results table or just image  
+    - [x] 6.4 Robustness
+        - [x] Add the text from paper 
+        - [x] Add regression results table or just image  
+    - [x] 7 Discussion 
+        - [x] Text 
+- [ ] APPENDIX 
+    - [ ] A. Literature Overview
+    - [ ] B. Calorific Values
+    - [ ] C. Text Classification models: All training results
+    - [ ] D. Results - Additional information
+    - [ ] E. Additional figures
+- [ ] Organize the repo
+    - [ ] Seperate the Report/site and the research assets (figs, tables, code etc.)
